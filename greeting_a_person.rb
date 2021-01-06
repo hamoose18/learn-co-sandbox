d9ba8bfd7812e56e20fa2c3ddacd63b5fbee1219
arhamm ghazali <arhamm_ghazali93@hotmail.com>
2021-01-06 21:14:43 +0000

    Automatically backed up by Learn

---
+++ greeting_a_person.rb
@@ -0,0 +1,6 @@
+require 'pry'
+def greeting_a_person(name)
+  name = "Maria"
+puts "Hello, #{name}"
+binding.pry
+end 
---
+++ mappingish.rb
@@ -0,0 +1,10 @@
+def love_this_veggie?(vegetable)
+  if vegetable == "broccoli"
+    "Nah, thanks"
+  else
+    "I love it!"
+  end
+end 
+
+love_this_veggie?
+
